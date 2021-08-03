0882372659b4284fd96850c5884eb5cf7977b902
Prateek Deshmukh <prateek.deshmukh19@gmail.com>
2021-08-03 16:43:01 +0530

    added new 8ball command

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import discord
 import os
 import requests
 import json
-from sys import argv
+from discord.ext import commands
+import random
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '.')
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -17,16 +18,40 @@ def get_quote():
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('{0.user}, Indeed.'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def motivate(ctx):
+    quote = get_quote()
+    await ctx.send(quote)
 
-    if message.content.startswith('$motivate'):
-        quote = get_quote()
-        await message.channel.send(quote)
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'{round(client.latency*1000)}ms')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt.',
+                 'Yes - definitely',
+                 'You may rely on it',
+                 'As I see it, yes.',
+                 'Most likely',
+                 'Outlook good.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, try again.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now',
+                 'Concentrate and ask again.',
+                 "Don't count on it.",
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 token = os.environ.get('TOKEN')
 client.run(token)
